0457695375f93db3f97a9efdde34cb8cfd22f29c
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-06-08 18:38:14 -0600

    Add some basic styling to article details view

--- src/Components/ArticleDetails/ArticleDetails.js
+++ src/Components/ArticleDetails/ArticleDetails.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import './ArticleDetails.css'
 
 function ArticleDetails(props) {
   return (
     <div className='article-details'>
-      <img src={props.article.multimedia[0].url} />
+      <figure>
+        <img className="article-image" src={props.article.multimedia[0].url} alt={props.article.multimedia[0].caption} />
+        <figcaption>{props.article.multimedia[0].caption}</figcaption>
+      </figure>
       <h2>{props.article.title}</h2>
       <p>{props.article.byline}</p>
       <p>{props.article.abstract}</p>
